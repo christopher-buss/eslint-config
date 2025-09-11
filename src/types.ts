f21e99a3ef870480b2c7467d9f461a63f9086cf0
christopher-buss <christopher.buss+github@protonmail.com>
2025-09-11 19:59:44 +0100

    fix: update `typed-flat-config-item` to provide rules auto-completion

--- src/types.ts
+++ src/types.ts
@@ -347,7 +347,7 @@ export type ReactConfig = ESLintReactSettings &
 		filenameCase?: "kebabCase" | "pascalCase";
 	};
 
-export interface Rules extends RuleOptions {}
+export type Rules = Record<string, Linter.RuleEntry<any> | undefined> & RuleOptions;
 
 export interface SpellCheckConfig {
 	/**
@@ -369,15 +369,23 @@ export type StylisticConfig = Pick<
 	"indent" | "jsx" | "quotes" | "semi"
 > & { arrowLength?: number };
 
+/**
+ * An updated version of ESLint's `Linter.Config`, which provides autocompletion
+ * for `rules` and relaxes type limitations for `plugins` and `rules`, because
+ * many plugins still lack proper type definitions.
+ */
 export type TypedFlatConfigItem = Omit<Linter.Config<Linter.RulesRecord & Rules>, "plugins"> & {
-	// Relax plugins type limitation, as most of the plugins do not have
-	// correct type info yet.
 	/**
-	 * An object containing a name-value mapping of plugin names to plugin
-	 * objects. When `files` is specified, these plugins are only available to
-	 * the matching files.
+	 * An object containing a name-value mapping of plugin names to plugin objects.
+	 * When `files` is specified, these plugins are only available to the matching files.
 	 *
 	 * @see [Using plugins in your configuration](https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new#using-plugins-in-your-configuration)
 	 */
 	plugins?: Record<string, any>;
+
+	/**
+	 * An object containing the configured rules. When `files` or `ignores` are
+	 * specified, these rule configurations are only available to the matching files.
+	 */
+	rules?: Rules;
 };
