2998327a46121818e6df116e525e4760d7e1a259
Christopher Buss <christopher.buss+github@protonmail.com>
2025-09-18 16:18:48 +0100

    chore(deps): update @eslint-react/eslint-plugin to v1.53.1 and eslint to v9.35.0

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,6 +1,6 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "1.52.5",
-  "eslint": "9.33.0",
+  "@eslint-react/eslint-plugin": "1.53.1",
+  "eslint": "9.35.0",
   "eslint-plugin-jest": "29.0.1",
   "eslint-plugin-react-roblox-hooks": "5.1.0-rbx.1"
 }
