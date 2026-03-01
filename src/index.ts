b6e4da9fbe2bbdf1601b6411e607410da45465a8
christopher-buss <christopher.buss+github@protonmail.com>
2026-03-01 13:11:51 +0000

    feat: export environment utilities

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from "./factory";
 export { isentinel as default } from "./factory";
 export * from "./globs";
 export * from "./types";
+
+export { isInEditorEnvironment } from "./utils";
+export { isInAgentSession } from "./utils";
+export { isInGitHooksOrLintStaged } from "./utils";
