46c4ab6aaeb916a17916a08450d67565b19ec585
christopher-buss <christopher.buss+github@protonmail.com>
2026-07-01 17:34:06 +0100

    feat(cease): add readonly props rule

--- src/configs/cease-nonsense.ts
+++ src/configs/cease-nonsense.ts
@@ -1,18 +1,62 @@
-import type { OptionsIsInEditor, OptionsStylistic, TypedFlatConfigItem } from "../types.ts";
-import { interopDefault } from "../utils.ts";
+import { GLOB_MARKDOWN, GLOB_TS, GLOB_TSX } from "../globs.ts";
+import type {
+	OptionsComponentExtensions,
+	OptionsFiles,
+	OptionsIsInEditor,
+	OptionsOverridesTypeAware,
+	OptionsStylistic,
+	OptionsTypeScriptParserOptions,
+	OptionsTypeScriptWithTypes,
+	TypedFlatConfigItem,
+} from "../types.ts";
+import { getTsConfig, interopDefault } from "../utils.ts";
 
 export async function ceaseNonsense(
-	options: OptionsIsInEditor & OptionsStylistic = {},
+	options: OptionsComponentExtensions &
+		OptionsFiles &
+		OptionsIsInEditor &
+		OptionsOverridesTypeAware &
+		OptionsStylistic &
+		OptionsTypeScriptParserOptions &
+		OptionsTypeScriptWithTypes = {},
 ): Promise<Array<TypedFlatConfigItem>> {
-	const { isInEditor = false, stylistic = true } = options;
+	const {
+		componentExts: componentExtensions = [],
+		isInEditor = false,
+		overridesTypeAware = {},
+		stylistic = true,
+		typeAware = true,
+	} = options;
 
 	const pluginCeaseNonsense = await interopDefault(
 		import("@pobammer-ts/eslint-cease-nonsense-rules"),
 	);
 
+	const files = options.files ?? [
+		GLOB_TS,
+		GLOB_TSX,
+		...componentExtensions.map((extension) => `**/*.${extension}`),
+	];
+
+	const filesTypeAware = options.filesTypeAware ?? [GLOB_TS, GLOB_TSX];
+	const ignoresTypeAware = options.ignoresTypeAware ?? [`${GLOB_MARKDOWN}/**`];
+	const tsconfigPath = typeAware ? getTsConfig(options.tsconfigPath) : undefined;
+	const isTypeAware = tsconfigPath !== undefined;
+
+	const typeAwareRules: TypedFlatConfigItem["rules"] = {
+		"cease-nonsense/prefer-read-only-props": "error",
+	};
+
 	return [
 		{
+			name: "isentinel/cease-nonsense/setup",
+			plugins: {
+				"cease-nonsense": pluginCeaseNonsense,
+			},
+		},
+		{
 			name: "isentinel/cease-nonsense",
+			files,
 			plugins: {
 				"cease-nonsense": pluginCeaseNonsense,
 			},
@@ -31,5 +75,18 @@ export async function ceaseNonsense(
 					: {}),
 			},
 		},
+		...(isTypeAware
+			? [
+					{
+						name: "isentinel/typescript/rules-type-aware",
+						files: filesTypeAware,
+						ignores: ignoresTypeAware,
+						rules: {
+							...typeAwareRules,
+							...overridesTypeAware,
+						},
+					},
+				]
+			: []),
 	];
 }
