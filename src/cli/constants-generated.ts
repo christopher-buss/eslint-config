716564fabb2ea8b88c7327260958a4ed2b1cab50
christopher-buss <christopher.buss+github@protonmail.com>
2026-07-03 14:08:29 +0100

    feat(unicorn): update to v70

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,6 +1,6 @@
 export const versionsMap = {
   "eslint": "10.6.0",
-  "eslint-plugin-jest": "29.15.3",
+  "eslint-plugin-jest": "29.15.4",
   "eslint-plugin-react-jsx": "5.10.0",
   "eslint-plugin-react-naming-convention": "5.10.0",
   "eslint-plugin-react-x": "5.10.0"
