b158a816979c60fb2a8ae2ca857b6eaee11133b4
christopher-buss <christopher.buss+github@protonmail.com>
2026-07-24 01:38:19 +0100

    chore: update constants

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,7 +1,7 @@
 export const versionsMap = {
   "eslint": "10.7.0",
-  "eslint-plugin-jest": "29.15.4",
-  "eslint-plugin-react-jsx": "5.17.2",
-  "eslint-plugin-react-naming-convention": "5.17.2",
-  "eslint-plugin-react-x": "5.17.2"
+  "eslint-plugin-jest": "29.15.5",
+  "eslint-plugin-react-jsx": "5.17.3",
+  "eslint-plugin-react-naming-convention": "5.17.3",
+  "eslint-plugin-react-x": "5.17.3"
 }
